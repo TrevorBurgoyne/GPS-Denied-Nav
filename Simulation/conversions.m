434289be06fac5ee4695dece0d73b6b8e8815dc1
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-10-25 14:47:16 -0500

    Add accelparams

--- Simulation/conversions.m
+++ Simulation/conversions.m
@@ -1,7 +1,10 @@
 %% Unit conversions
 % GPS Denied Navigation, AEM 4331 Fall 2022
-
 classdef conversions
+    properties (Constant)
+        g  = 9.81;   % m/s^2
+    end
+    
     methods (Static)
         function ret = deg_per_hr_to_rad_per_sec(val)
             % deg/hr -> rad/s
@@ -11,6 +14,16 @@ classdef conversions
         function ret = deg_per_sqrt_hr_to_rad_per_sqrt_sec(val)
             % deg/sqrt(hr) -> rad/sqrt(s) = (rad/s)/sqrt(Hz)
             ret = (val*pi)/(180*60); 
+        end
+        
+        function ret = mg_to_m_per_s_squared(val)
+            % mg -> m/s^2
+            ret = (val/1000)*conversions.g;
+        end
+        
+        function ret = fps_per_sqrt_hr_to_m_per_sec_per_sqrt_sec(val)
+            % fps/sqrt(hr) -> (m/s)/sqrt(s) = m/s^2/sqrt(Hz)
+            ret = (val*0.3048)/(60);
         end
     end
     
--- Simulation/imu_model.m
+++ Simulation/imu_model.m
@@ -1,9 +1,8 @@
 %% IMU Model
 % GPS Denied Navigation, AEM 4331 Fall 2022
+% HG1125CA01 Performance
 
-%% HG1125CA01 Performance
-
-% Gryo
+%% Gryo
 gyro_bias_repeatability    = 120; % deg/hr
 gyro_bias_in_run_stability = 7;   % deg/hr
 gyro_arw                   = .3;  % deg/sqrt(hr)
@@ -13,14 +12,24 @@ gyro_bias_instability = conversions.deg_per_hr_to_rad_per_sec(gyro_bias_in_run_s
 gyro_noise_density    = conversions.deg_per_sqrt_hr_to_rad_per_sqrt_sec(gyro_arw);         % rad/sqrt(s)
 
 
-% Init params object
+% Init gyroparams object
 gyro_params = gyroparams(...
     'BiasInstability', gyro_bias_instability, ... % rad/s, Instability of the bias offset 
     'NoiseDensity',    gyro_noise_density     ... % rad/sqrt(s), Power spectral density of sensor noise                               
 );
 
-% Accelerometer
+%% Accelerometer
 acc_bias_repeatability     = 1.5; % mg
 acc_bias_in_run_stability  = 7;   % mg
 acc_vrw                    = .18; % fps/sqrt(hr)
+
+% Convert to param units
+acc_bias_instability = conversions.mg_to_m_per_s_squared(acc_bias_in_run_stability);   % m/s^2
+acc_noise_density    = conversions.fps_per_sqrt_hr_to_m_per_sec_per_sqrt_sec(acc_vrw); % m/s/sqrt(s) or m/s^2/sqrt(Hz)
+
+% Init accelparams object
+accel_params = accelparams(...
+    'BiasInstability', acc_bias_instability, ... % m/s^2, Instability of the bias offset 
+    'NoiseDensity',    acc_noise_density     ... % m/s^2/sqrt(Hz), Power spectral density of sensor noise 
+);
 
