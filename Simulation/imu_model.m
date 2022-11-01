b6d7f96ed6fb8920201aa8b305807210417a2792
TrevorBurgoyne <trevorburgoyne@gmail.com>
2022-11-01 13:54:07 -0500

    Make imu_model callable

--- Simulation/imu_model.m
+++ Simulation/imu_model.m
@@ -1,84 +1,60 @@
 %% IMU Model
 % GPS Denied Navigation, AEM 4331 Fall 2022
-% HG1125CA01 Performance
-
-%% Gryo
-gyro_bias_repeatability    = 120; % deg/hr
-gyro_bias_in_run_stability = 7;   % deg/hr
-gyro_arw                   = 0.3;  % deg/sqrt(hr)
-
-% Convert to param units
-gyro_bias_instability = conversions.deg_per_hr_to_rad_per_sec(gyro_bias_in_run_stability); % rad/s
-gyro_noise_density    = conversions.deg_per_sqrt_hr_to_rad_per_sqrt_sec(gyro_arw);         % rad/sqrt(s)
-
-
-% Init gyroparams object
-gyro_params = gyroparams(...
-    'BiasInstability', gyro_bias_instability, ... % rad/s, Instability of the bias offset 
-    'NoiseDensity',    gyro_noise_density     ... % rad/sqrt(s), Power spectral density of sensor noise                               
-);
-
-%% Accelerometer
-acc_bias_repeatability     = 1.5; % mg
-acc_bias_in_run_stability  = 7;   % mg
-acc_vrw                    = .18; % fps/sqrt(hr)
-
-% Convert to param units
-acc_bias_instability = conversions.mg_to_m_per_s_squared(acc_bias_in_run_stability);   % m/s^2
-acc_noise_density    = conversions.fps_per_sqrt_hr_to_m_per_sec_per_sqrt_sec(acc_vrw); % m/s/sqrt(s) or m/s^2/sqrt(Hz)
-
-% Init accelparams object
-accel_params = accelparams(...
-    'BiasInstability', acc_bias_instability, ... % m/s^2, Instability of the bias offset 
-    'NoiseDensity',    acc_noise_density     ... % m/s^2/sqrt(Hz), Power spectral density of sensor noise 
-);
-
+% Parameters taken from HG1125CA01 datasheet: 
+% https://aerospace.honeywell.com/content/dam/aerobt/en/documents/learn/products/sensors/brochures/N61-2427-000-001_HG1125_HG1126-bro.pdf
 
 %% IMU
-N = 1000;
-Fs = 100; % Hz, sampling rate
-Fc = .25; % sinusoidal frequency
+% Usage
+function imu = imu_model(sample_rate)
+    % imuSensor model for a HG1125CA01 sensor.
+    %   USAGE: `imu = imu_model();` will initialize a imuSensor model
+    %   object with the default HG1125CA01 parameters. The sample rate (Fs)
+    %   can also be adjusted using `imu = imu_model(Fs);`
+        
+    % Gryo
+    gyro_bias_repeatability    = 120; % deg/hr
+    gyro_bias_in_run_stability = 7;   % deg/hr
+    gyro_arw                   = 0.3;  % deg/sqrt(hr)
+    
+    % Convert to param units
+    gyro_bias_instability = conversions.deg_per_hr_to_rad_per_sec(gyro_bias_in_run_stability); % rad/s
+    gyro_noise_density    = conversions.deg_per_sqrt_hr_to_rad_per_sqrt_sec(gyro_arw);         % rad/sqrt(s
+    
+    % Init gyroparams object
+    gyro_params = gyroparams(...
+        'BiasInstability', gyro_bias_instability, ... % rad/s, Instability of the bias offset 
+        'NoiseDensity',    gyro_noise_density     ... % rad/sqrt(s), Power spectral density of sensor noise                               
+    );
 
-t = (0:(1/Fs):((N-1)/Fs)).';
-acc = zeros(N,3);
-angvel = zeros(N,3);
-angvel(:,1) = sin(2*pi*Fc*t);
+    % Accelerometer
+    acc_bias_repeatability     = 1.5; % mg
+    acc_bias_in_run_stability  = 7;   % mg
+    acc_vrw                    = .18; % fps/sqrt(hr)
+    
+    % Convert to param units
+    acc_bias_instability = conversions.mg_to_m_per_s_squared(acc_bias_in_run_stability);   % m/s^2
+    acc_noise_density    = conversions.fps_per_sqrt_hr_to_m_per_sec_per_sqrt_sec(acc_vrw); % m/s/sqrt(s) or m/s^2/sqrt(Hz)
+    
+    % Init accelparams object
+    accel_params = accelparams(...
+        'BiasInstability', acc_bias_instability, ... % m/s^2, Instability of the bias offset 
+        'NoiseDensity',    acc_noise_density     ... % m/s^2/sqrt(Hz), Power spectral density of sensor noise 
+    );
 
-imu = imuSensor(...
-  'accel-gyro-mag', ...
-  'ReferenceFrame', 'NED', ...
-  'SampleRate', Fs, ...
-  'Gyroscope', gyro_params, ...
-  'Accelerometer', accel_params ...
-);
+    Fs = 300; % Hz, default sampling rate. 600Hz/100Hz (configurable to 1800Hz/300Hz)
+    
+    % Constructor
+    if nargin > 0
+        Fs = sample_rate;
+    end
 
-[accelData, gyroData, magData] = imu(acc, angvel);
-
-% subplot(3, 1, 1)
-% plot(t, accelData)
-% title('Accelerometer')
-% xlabel('s')
-% ylabel('m/s^2')
-% legend('x','y','z')
-% 
-% subplot(3, 1, 2)
-% plot(t, gyroData)
-% title('Gyroscope')
-% xlabel('s')
-% ylabel('rad/s')
-% legend('x','y','z')
-% 
-% subplot(3, 1, 3)
-% plot(t, magData)
-% title('Magnetometer')
-% xlabel('s')
-% ylabel('uT')
-% legend('x','y','z')
-
-plot(t, angvel(:,1), '--', t, gyroData(:,1))
-title('Gyroscope')
-xlabel('s')
-ylabel('rad/s')
-legend('x (truth)','x (gyro)')
-
+    imu = imuSensor(...
+      'accel-gyro-mag', ...
+      'ReferenceFrame', 'NED', ...
+      'SampleRate', Fs, ...
+      'Gyroscope', gyro_params, ...
+      'Accelerometer', accel_params ...
+    );
+      
+end
 
---
+++ Simulation/imu_test.m
@@ -0,0 +1,41 @@
+%% IMU Test
+% GPS Denied Navigation, AEM 4331 Fall 2022
+
+imu = imu_model();
+N = 1000;
+Fc = .25; % sinusoidal frequency
+t = (0:(1/imu.SampleRate):((N-1)/imu.SampleRate)).';
+acc = zeros(N,3);
+angvel = zeros(N,3);
+angvel(:,1) = sin(2*pi*Fc*t);
+
+[accelData, gyroData, magData] = imu(acc, angvel);
+
+subplot(3, 1, 1)
+plot(t, accelData)
+title('Accelerometer')
+xlabel('s')
+ylabel('m/s^2')
+legend('x','y','z')
+
+subplot(3, 1, 2)
+plot(t, gyroData)
+title('Gyroscope')
+xlabel('s')
+ylabel('rad/s')
+legend('x','y','z')
+
+subplot(3, 1, 3)
+plot(t, magData)
+title('Magnetometer')
+xlabel('s')
+ylabel('uT')
+legend('x','y','z')
+
+% plot(t, angvel(:,1), '--', t, gyroData(:,1))
+% title('Gyroscope')
+% xlabel('s')
+% ylabel('rad/s')
+% legend('x (truth)','x (gyro)')
+
+
